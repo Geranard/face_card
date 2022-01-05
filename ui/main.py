c5a7efc93754594618c7afebeb180b968eea6d7d
tivoner <regie.tjandranata2002@gmail.com>
2022-01-05 16:51:35 +0700

    Update main.py

--- ui/main.py
+++ ui/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from login import Login
+import os
 
 BLACK = "#1C2626"
 WHITE = "#FFFFFF"
@@ -8,6 +9,24 @@ PURPLE = "#552D96"
 GREEN = "#00FF00"
 GRAY = "#C4C4C4"
 FONT = "Poppins"
+
+try:
+    with open("../data/class_student.json", mode="r") as file:
+        pass
+except:
+    with open("../data/class_student.json", mode="w") as file:
+        file.write("{}")
+
+try:
+    with open("../data/student.json", mode="r") as file:
+        pass
+except:
+    with open("../data/student.json", mode="w") as file:
+        file.write("{}")
+
+path_exists = os.path.exists("../data/student_image")
+if path_exists == False:
+    os.makedirs("../data/student_image")
 
 main_window = Tk()
 main_window.title("FACE CARD MANAGER")
